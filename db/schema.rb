730877119c50b38fc2863cc3ae4c30523acc2e61
wallyali <abdishwak@gmail.com>
2014-03-08 01:57:26 -0600

    add user model validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140307055251) do
+ActiveRecord::Schema.define(version: 20140308075010) do
 
   create_table "users", force: true do |t|
     t.string   "name"
