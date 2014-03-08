d3730421c884727ec86366c77ba8eba2a8eb57d7
wallyali <abdishwak@gmail.com>
2014-03-08 13:27:29 -0600

    Add password and encrypted password attributes to the User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140308075010) do
+ActiveRecord::Schema.define(version: 20140308191639) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
   end
 
 end
